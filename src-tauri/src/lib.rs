d282ae1f1d20311bb03b311dfeb14b67891d5ca5
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2024-11-22 15:24:51 -0800

    :sparkles: image upload started

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,24 +1,23 @@
 use std::process::Command;
 
-
-
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn open_vscode_project(name: &str) -> (){
+fn open_vscode_project(name: &str) -> () {
     println!("running CLI command");
     let command = Command::new("cmd")
-            .arg("/C")
-            .arg("code")
-            .arg("-n")
-            .arg(name)
-            .output()
-            .expect("CLI command failed");
+        .arg("/C")
+        .arg("code")
+        .arg("-n")
+        .arg(name)
+        .output()
+        .expect("CLI command failed");
     println!("{} {:?}", command.status, command.stdout);
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![open_vscode_project])
         .run(tauri::generate_context!())
--- src/screens/ProjectsScreen/AddBlockSidebar/AddBlockSidebar.tsx
+++ src/screens/ProjectsScreen/AddBlockSidebar/AddBlockSidebar.tsx
@@ -2,6 +2,8 @@ import React, { useRef, useState } from "react";
 import useAppStore from "../../../store/store";
 import "./AddBlockSidebar.css";
 import { Block, BLOCK_TYPES } from "../../../store/types";
+import { open } from "@tauri-apps/plugin-dialog";
+import { convertFileSrc } from "@tauri-apps/api/core";
 
 type Props = {};
 
@@ -10,6 +12,8 @@ const AddBlockSidebar = (props: Props) => {
   const [type, setType] = useState(BLOCK_TYPES[0]);
   const nameRef = useRef<HTMLInputElement | null>(null);
   const commandRef = useRef<HTMLInputElement | null>(null);
+  const imageRef = useRef<HTMLInputElement | null>(null);
+  const [imagePath, setImagePath] = useState("");
 
   const handleTypeChange = (e) => {
     console.log("changing project", e.currentTarget.value);
@@ -19,30 +23,67 @@ const AddBlockSidebar = (props: Props) => {
   const handleCreateBlock = () => {
     if (nameRef.current == null) return;
     if (commandRef.current == null) return;
-    const newBlock: Block = {
-      name: nameRef.current.value,
-      type,
-      project: currentProject,
-      command: commandRef.current.value,
-    };
-    addBlock(newBlock);
+    if (imageRef.current == null) return;
 
-    // Clear inputs
-    nameRef.current.value = "";
-    commandRef.current.value = "";
+    console.log("image", imageRef.current, imageRef.current.value);
+
+    // const newBlock: Block = {
+    //   name: nameRef.current.value,
+    //   type,
+    //   project: currentProject,
+    //   command: commandRef.current.value,
+    // };
+    // addBlock(newBlock);
+
+    // // Clear inputs
+    // nameRef.current.value = "";
+    // commandRef.current.value = "";
+  };
+
+  const handleSelectImage = async () => {
+    let filepath = await open({
+      multiple: false,
+      directory: false,
+      filters: [
+        {
+          name: "",
+          extensions: ["jpg", "jpeg", "gif", "png", "svg", "webp"],
+        },
+      ],
+    });
+
+    if (!filepath) return;
+    console.log("file path", filepath);
+
+    // Save image URL
+    setImagePath(filepath);
   };
 
   const openStatus = projectSidebar ? "open" : "closed";
+  const imageSrc = imagePath != "" ? convertFileSrc(imagePath) : "";
   return (
     <div className={`AddBlockSidebar ${openStatus}`}>
-      <input ref={nameRef} type="text" placeholder="Name of block" />
-      <input ref={commandRef} type="text" placeholder="Command" />
-      <select value={type} onChange={handleTypeChange}>
-        {BLOCK_TYPES.map((blockType) => (
-          <option value={blockType}>{blockType}</option>
-        ))}
-      </select>
-      <button onClick={handleCreateBlock}>Create block</button>
+      <div className="content">
+        <input ref={nameRef} type="text" placeholder="Name of block" />
+        <input ref={commandRef} type="text" placeholder="Command" />
+        <select value={type} onChange={handleTypeChange}>
+          {BLOCK_TYPES.map((blockType) => (
+            <option value={blockType}>{blockType}</option>
+          ))}
+        </select>
+        <div
+          className="image-preview"
+          style={{ backgroundImage: `url(${imageSrc})` }}
+        />
+        {/* <input
+        ref={imageRef}
+        type="file"
+        placeholder="Cover image"
+        accept=".jpg,.jpeg,.gif,.png,.svg,.webp"
+      /> */}
+        <button onClick={handleSelectImage}>Select new image</button>
+        <button onClick={handleCreateBlock}>Create block</button>
+      </div>
     </div>
   );
 };
