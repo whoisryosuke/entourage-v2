a60c74d769eca2ab6c78c802cc7603f85157db40
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2024-11-23 00:31:13 -0800

    :zap: persist store

--- src/store/store.ts
+++ src/store/store.ts
@@ -27,64 +27,66 @@ export const useAppStore = create<AppState>()(
     // Optional persist
     // This saves Zustand state when you close browser
     // Good in some cases, but not in others, especially prototyping
-    // persist(
+    persist(
+      (set) => ({
+        // We keep the NextJS router in state because it's undefined in most components
+        currentProject: "",
+        setCurrentProject: (currentProject) =>
+          set(() => ({
+            currentProject,
+          })),
 
-    (set) => ({
-      // We keep the NextJS router in state because it's undefined in most components
-      currentProject: "",
-      setCurrentProject: (currentProject) =>
-        set(() => ({
-          currentProject,
-        })),
+        projectSidebar: false,
+        toggleProjectSidebar: () =>
+          set((state) => ({
+            projectSidebar: !state.projectSidebar,
+          })),
 
-      projectSidebar: false,
-      toggleProjectSidebar: () =>
-        set((state) => ({
-          projectSidebar: !state.projectSidebar,
-        })),
+        editMode: false,
+        toggleEditMode: () =>
+          set((state) => ({
+            editMode: !state.editMode,
+          })),
 
-      editMode: false,
-      toggleEditMode: () =>
-        set((state) => ({
-          editMode: !state.editMode,
-        })),
+        projects: [],
+        addProject: (project) =>
+          set((state) => ({
+            projects: [...state.projects, project],
+          })),
+        removeProject: (projectIndex) =>
+          set((state) => {
+            const projectId = state.projects[projectIndex].id;
+            return {
+              projects: [
+                ...state.projects.filter((_, index) => index !== projectIndex),
+              ],
+              blocks: [
+                ...state.blocks.filter((block) => block.project !== projectId),
+              ],
+            };
+          }),
 
-      projects: [],
-      addProject: (project) =>
-        set((state) => ({
-          projects: [...state.projects, project],
-        })),
-      removeProject: (projectIndex) =>
-        set((state) => {
-          const projectId = state.projects[projectIndex].id;
-          return {
-            projects: [
-              ...state.projects.filter((_, index) => index !== projectIndex),
-            ],
-            blocks: [
-              ...state.blocks.filter((block) => block.project !== projectId),
-            ],
-          };
-        }),
+        blocks: [],
+        addBlock: (block) =>
+          set((state) => ({
+            blocks: [...state.blocks, block],
+          })),
+        removeBlock: (blockId) =>
+          set((state) => ({
+            blocks: [...state.blocks.filter((_, index) => index !== blockId)],
+          })),
 
-      blocks: [],
-      addBlock: (block) =>
-        set((state) => ({
-          blocks: [...state.blocks, block],
-        })),
-      removeBlock: (blockId) =>
-        set((state) => ({
-          blocks: [...state.blocks.filter((_, index) => index !== blockId)],
-        })),
+        // Add any default values for app-wide state here
+        // e.g. game start logic, points/score, etc
+        // gameStarted: true,
+        // points: 100,
+      }),
 
-      // Add any default values for app-wide state here
-      // e.g. game start logic, points/score, etc
-      // gameStarted: true,
-      // points: 100,
-    })
-
-    // END: Optional persist
-    // )
+      // END: Optional persist
+      {
+        name: "entourage-app",
+      }
+    )
   )
 );
 
