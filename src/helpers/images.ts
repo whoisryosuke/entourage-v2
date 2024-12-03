e22f25e5656d021870fb0a6716da2be875159813
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2024-12-02 17:29:57 -0800

    :hammer: cleaner

--- src/helpers/images.ts
+++ src/helpers/images.ts
@@ -1,8 +1,9 @@
 import { convertFileSrc } from "@tauri-apps/api/core";
-import { appLocalDataDir } from "@tauri-apps/api/path";
+import { appLocalDataDir, join } from "@tauri-apps/api/path";
 
 export const getImage = async (imageName: string) => {
-  const imagePath = `${await appLocalDataDir()}/${imageName}`;
+  const appDataDirPath = await appLocalDataDir();
+  const imagePath = await join(appDataDirPath, imageName);
   const newImageSrc = convertFileSrc(imagePath);
   return newImageSrc;
 };
